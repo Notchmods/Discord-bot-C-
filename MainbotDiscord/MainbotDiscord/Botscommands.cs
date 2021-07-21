915e80a9b5117faee268333403870438a6d93a41
Notchmods <Nomics3000@gmail.com>
2021-07-22 08:29:33 +1000

    Discord bot with C#

---
+++ MainbotDiscord/MainbotDiscord/Botscommands.cs
@@ -0,0 +1,90 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Globalization;
+
+namespace MainbotDiscord
+{
+    public class Botscommands : ModuleBase<SocketCommandContext>
+    {
+        public DateTime thedate = DateTime.Now;
+        string[] Colors = new string[] { "Red", "Blue", "Green", "Orange", "Purple", "Yellow", "Lime", "Aqua", "Turquoise", "pink", "Black", "Brown" };
+        public bool KSIrapefaceunlock;
+        [Command("Hello")]
+        public async Task Hello()
+        {
+            await ReplyAsync("Hello how are you?");
+        }
+        [Command("Hi")]
+        public async Task Hi()
+        {
+            await ReplyAsync("Hello how are you?");
+        }
+        [Command("Fuck you")]
+        public async Task Fuckyou()
+        {
+            await ReplyAsync("Haha you think I was gonna respond the same commands back to you but Fuck you too", isTTS: true);
+        }
+        [Command("How are you")]
+        public async Task Howareyou()
+        {
+            await ReplyAsync("I am fine thank you now what do you need from me?");
+        }
+
+        [Command("Show me KSI Rapeface")]
+        public async Task KSIRapeface()
+        {
+            await ReplyAsync("Here you go:https://i.redd.it/zuqwgy86xsa41.jpg");
+        }
+        [Command("Generate Random numbers for me")]
+        public async Task Generatenumber()
+        {
+            Random Randnumbers = new Random();
+            await ReplyAsync(Randnumbers.Next(-1000, 1000).ToString());
+        }
+        [Command("Generate Random colors for me")]
+        public async Task GenerateColors()
+        {
+            Random Randomnums = new Random();
+            await ReplyAsync("Here is the color " + Colors[Randomnums.Next(0, 13)].ToString(), isTTS: true);
+        }
+        [Command("What's the time and date")]
+        public async Task Time()
+        {
+            await ReplyAsync(thedate.ToString(),isTTS: true);
+        }
+        [Command("Help")]
+        public async Task Help()
+        {
+            await ReplyAsync("Here are the commands to use: What's the time and date,How long do you operate for,Generate Random colors for me,Generate Random numbers for me,How are you,Hello,Show me KSI Rapeface. I know these commands are limited and Notchmods is adding them");
+        }
+        [Command("You're a penis")]
+        public async Task Youareapenis()
+        {
+            await ReplyAsync("No you have a little penis");
+        }
+        [Command("You are a penis")]
+        public async Task YouRpenis()
+        {
+            await ReplyAsync("No you have a little penis", isTTS: true);
+        }
+        [Command("How long do you operate for")]
+        public async Task Oper8()
+        {
+            await ReplyAsync("I operate when I want cupcakes", isTTS: true);
+        }
+
+        [Command("Spr")]
+        public async Task Sprache()
+        {
+            Console.WriteLine("Type in your commands and I'll speak em");
+            var Keys = Console.ReadLine();
+            await ReplyAsync(Keys);
+        }
+    }
+}
---
+++ MainbotDiscord/MainbotDiscord/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MainbotDiscord
+{
+    class Program
+    {
+        public Botscommands Commands;
+        //Declare variables socket client
+        private DiscordSocketClient Socketclients;
+        //Declaring framework provider for building Discord Commands
+        private CommandService CS;
+        //Declaring mechanism for retrieving a service object
+        private IServiceProvider ISP;
+        //Run methods when the bot is online;
+        static void Main(string[] args) => new Program().Runbotasync().GetAwaiter().GetResult();
+        //When the bot is runned these functions are performed
+        public async Task Runbotasync()
+        {
+            //Declare the variables above
+            Socketclients = new DiscordSocketClient();
+            CS = new CommandService();
+            ISP = new ServiceCollection()
+                // Make sure one instance of these commands are running
+                .AddSingleton(Socketclients)
+                .AddSingleton(CS)
+                //Creates IServiceProvider from IServiceCollection
+                .BuildServiceProvider();
+            //Declaring the discord tokens into a string
+            string Tokens = "ODQ5MjAzNzE5NjI3MzQxODQ0.YLXwmA.rg1BY9nxWTWFzZ3MBOr3K9DzM0c";
+            Socketclients.Log += Socketclients_Log;
+            await Registercommandsasync();
+            await Socketclients.LoginAsync(Discord.TokenType.Bot, Tokens);
+            //Start the socket clients of the bot
+            await Socketclients.StartAsync();
+            //Delay the task
+            await Task.Delay(-1);
+        }
+
+        private Task Socketclients_Log(Discord.LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        //Register the commands once online.
+        public async Task Registercommandsasync()
+        {
+            //If message received Discord websocket clients is gonna Handle the Commands
+            Socketclients.MessageReceived += HandleCommandasync;
+            await CS.AddModulesAsync(Assembly.GetEntryAssembly(),ISP);
+        }
+            
+        private async Task HandleCommandasync(SocketMessage arg)
+        {
+            //Set SocketUserMessage as messagges
+            var messagess = arg as SocketUserMessage;
+            //Declare SocketCommandContext;
+            var context = new SocketCommandContext(Socketclients,messagess);
+            int argPos = 0;
+            //If the author is bot then don't respond at all
+            if(messagess.Author.IsBot == true)
+            {
+                return;
+            }
+            //If the users queries contain MB then the bot will respond to its questions
+            if (messagess.HasStringPrefix("?", ref argPos))
+            {
+                var results = await CS.ExecuteAsync(context, argPos, ISP);
+                if (!results.IsSuccess) 
+                {
+                    Console.WriteLine(results.ErrorReason);
+                }
+            }
+            if(messagess.Content == "K")
+            {
+                while (true)
+                {
+                    Console.WriteLine("Type in your commands and I'll speak em");
+                    var Keys = Console.ReadLine();
+                    if (Keys == "Users")
+                    {
+                        var config = new DiscordSocketConfig
+                        {
+                            AlwaysDownloadUsers = true,
+                            MessageCacheSize = 100
+                        };
+                        Console.WriteLine();
+                    }else
+                    {
+                        await messagess.Channel.SendMessageAsync(Keys);
+                    }
+                }
+            }   
+        }
+    }
+}
